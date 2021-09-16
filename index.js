5c6cc224bedac1da65ad6d5e698c59b5cd2f374d
lordrickyz <rickyzheng@ymail.com>
2021-09-16 17:13:29 -0400

    Add Colors and Navigation button for links

--- index.js
+++ index.js
@@ -18,17 +18,36 @@ const colors = [
 
 
 let appName = document.getElementById('app-name');
-appName.innerText = apps[0]
-appName.style.color = colors[0]
+appName.innerText = apps[0];
+appName.style.color = colors[0];
+
+let linkName = document.getElementById('link-app');
+linkName.style.color = colors[0];
+linkName.style.textDecoration = 'underline'
+
+const linkers = document.getElementsByClassName('linkers')
 
 const toggleDark = () => {
+  for(let i = 0; i < linkers.length; i++) {
+    linkers[i].style.color = 'white';
+  }
+  linkName.style.textDecoration = 'none'
   document.body.style.backgroundColor = 'black';
   document.body.style.color = '#FFFFFF';
+  document.getElementsByClassName('side-btns')[0].style.color = 'white';
+  document.getElementsByClassName('side-btns')[1].style.color = 'white';
 }
 
 const toggleLight = () => {
+  for(let i = 0; i < linkers.length; i++) {
+    if (linkers[i].innerText != apps[0]) {
+      linkers[i].style.color = 'black';
+    }
+  }
   document.body.style.backgroundColor = 'white';
   document.body.style.color = 'black';
+  document.getElementsByClassName('side-btns')[0].style.color = 'black';
+  document.getElementsByClassName('side-btns')[1].style.color = 'black';
 }
 
 const rotateRight = () => {
@@ -38,6 +57,14 @@ const rotateRight = () => {
   colors.push(colors.shift())
   appName.style.color = colors[0]
 
+  if (apps[0] != 'grave') {
+    linkName.style.color = 'black'
+    linkName.style.textDecoration = 'none'
+    linkName = document.getElementById('link-' + apps[0])
+    linkName.style.color = colors[0]
+    linkName.style.textDecoration = 'underline'
+  }
+  
   apps[0] === 'grave' ? toggleDark() : toggleLight();
 
   return apps
@@ -47,9 +74,16 @@ const rotateLeft = () => {
   apps.unshift(apps.pop())
   appName.innerText = apps[0]
 
-  colors.push(colors.shift())
+  colors.unshift(colors.pop())
   appName.style.color = colors[0]
 
+  if (apps[0] != 'grave') {
+    linkName.style.color = 'black'
+    linkName.style.textDecoration = 'none'
+    linkName = document.getElementById('link-' + apps[0])
+    linkName.style.color = colors[0]
+    linkName.style.textDecoration = 'underline'
+  }
     
   apps[0] === 'grave' ? toggleDark() : toggleLight();
 
